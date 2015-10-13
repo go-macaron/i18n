5e728b6f45f60ecc6d4d95cfacadd337fb4cd182
Unknwon <u@gogs.io>
2015-10-13 03:33:08 -0400

    upgrade with macaron

--- i18n.go
+++ i18n.go
@@ -1,4 +1,4 @@
-// Copyright 2014 Unknwon
+// Copyright 2014 The Macaron Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License"): you may
 // not use this file except in compliance with the License. You may obtain
@@ -22,10 +22,10 @@ import (
 
 	"github.com/Unknwon/com"
 	"github.com/Unknwon/i18n"
-	"github.com/Unknwon/macaron"
+	"gopkg.in/macaron.v1"
 )
 
-const _VERSION = "0.0.7"
+const _VERSION = "0.1.0"
 
 func Version() string {
 	return _VERSION
